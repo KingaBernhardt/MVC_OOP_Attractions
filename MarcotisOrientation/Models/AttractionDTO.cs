e9cd0a055b08e24c594e6f01fdb548622e7dfd95
Kinga Bernhardt <kinga.bernhardt@gmail.com>
2018-11-15 15:27:30 +0000

    feat(macrotisOrientation):add models to project

---
+++ MarcotisOrientation/Models/AttractionDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MacrotisOrientation.Models
+{
+    public class AttractionDTO
+    {
+        public string result;
+        public int count;
+        public List<Attractions> attractions;
+
+        public AttractionDTO(string result, List<Attractions> input)
+        {
+            this.result = result;
+            this.count = input.Count();
+            this.attractions = input;
+        }
+
+    }
+}
