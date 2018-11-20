937159664a629ddf14903dd92582259356807180
Kinga Bernhardt <kinga.bernhardt@gmail.com>
2018-11-20 12:01:23 +0000

    feat(attractions):services

---
+++ MarcotisOrientation/Services/AttractionService.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MacrotisOrientation.Models;
+using MacrotisOrientation.Repositories;
+
+namespace MacrotisOrientation.Services
+{
+    public class AttractionService : IAttractionService
+    {
+        private AttractionsRepository attractionsRepository;
+
+        public AttractionService(AttractionsRepository attractionsRepository)
+        {
+            this.attractionsRepository = attractionsRepository;
+        }
+
+        public void AddAttraction(Attractions attraction)
+        {
+            attractionsRepository.Create(attraction);
+        }
+
+        public void Delete(Attractions attraction)
+        {
+            attractionsRepository.Delete(attraction);
+        }
+
+        public void EditAttraction(Attractions attraction)
+        {
+            attractionsRepository.Update(attraction);
+        }
+
+        public Attractions GetAttractionById(int id)
+        {
+            return attractionsRepository.GetElementById(id);
+        }
+
+        public List<Attractions> ListAllAttractions()
+        {
+            return attractionsRepository.Read();
+        }
+
+        public List<Attractions> ListByCategoryAndCity(string category, string city)
+        {
+            if (category is null && city is null)
+            {
+                return attractionsRepository.Read();
+            }
+            else if (city is null)
+            {
+                return attractionsRepository.Read().Where(attr => attr.Category.ToLower()
+                .Contains(category.ToLower())).ToList();
+            }
+            else if (category is null)
+            {
+                return attractionsRepository.Read().Where(attr => attr.City.ToLower()
+                .Contains(city.ToLower())).ToList();
+            }
+            return attractionsRepository.Read().Where(x => x.Category.ToLower()
+            .Contains(category.ToLower())
+            && x.City.ToLower().Contains(city.ToLower())).ToList();
+        }
+
+        public AttractionDTO CreateDTOBySearch(string category, string city)
+        {
+            AttractionDTO attractionDTO =
+                new AttractionDTO("ok", ListByCategoryAndCity(category, city));
+            return attractionDTO;
+        }
+    }
+}
---
+++ MarcotisOrientation/Services/IAttractionService.cs
@@ -0,0 +1,19 @@
+﻿using MacrotisOrientation.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MacrotisOrientation.Services
+{
+    public interface IAttractionService
+    {
+        List<Attractions> ListAllAttractions();
+        void AddAttraction(Attractions attraction);
+        void EditAttraction(Attractions attraction);
+        Attractions GetAttractionById(int id);
+        void Delete(Attractions attraction);
+        List<Attractions> ListByCategoryAndCity(string category, string city);
+        AttractionDTO CreateDTOBySearch(string category, string city);
+    }
+}
